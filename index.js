78e8c011697fa987f03646baf4ed2278edf907d8
littlephillips <phillipkinuthia72@gmail.com>
2022-06-30 03:05:03 +0300

    modified index.js file

--- index.js
+++ index.js
@@ -1,5 +1,68 @@
 /* Your Code Here */
 
+function createEmployeeRecord(employee){
+  return {
+  
+    firstName :employee[0],
+    familyName : employee[1],
+    title : employee[2],
+    payPerHour: employee[3],
+    timeInEvents: [],
+    timeOutEvents: []
+  
+  }
+}
+
+function createEmployeeRecords(employeeData){
+  return employeeData.map(employee => {
+     return  createEmployeeRecord(employee)
+    })
+  }
+
+function createTimeInEvent ( dateStamp){
+  let [date, hour] = dateStamp.split(" ")
+
+    this.timeInEvents.push({
+    type : "TimeIn",
+    hour : parseInt(hour, 10),
+    date,
+  })
+  return this;
+}
+
+
+function createTimeOutEvent( dateStamp){
+  let [date, hour] = dateStamp.split(" ")
+
+    this.timeOutEvents.push({
+    type : "TimeOut",
+    hour : parseInt(hour, 10),
+    date,
+  })
+  return this;
+}
+
+function hoursWorkedOnDate(specificDate){
+  let hoursWorked =this.timeInEvents.find(event => {
+    return event.date === specificDate;
+  })
+
+  let hoursOff = this.timeOutEvents.find(event => {
+    return event.date === specificDate;
+  })
+  return (hoursOff.hour - hoursWorked.hour) / 100
+ }
+
+
+function wagesEarnedOnDate ( specificDate){
+  let wagesOwed = hoursWorkedOnDate.call(this, specificDate)
+  * this.payPerHour;
+  return parseFloat(wagesOwed.toString())
+}
+
+
+
+
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
@@ -20,4 +83,13 @@ const allWagesFor = function () {
 
     return payable
 }
+
+
+
+function calculatePayroll (){
+    let sumOwed = this.allWagesFor.reduce((salary, days) => {
+     return salary + wagesEarnedOnDate(days)
+   
+    })
+   }
 
